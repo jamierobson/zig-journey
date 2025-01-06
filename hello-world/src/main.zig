0f3a23f90fb05790c98aba72a7a8901c01357587
Jamie Robson <jamierobson@users.noreply.github.com>
2025-01-06 23:16:40 +0100

    playing to get the guessing game going. Now to get rid of the delimeter and we will be golden

--- hello-world/src/main.zig
+++ hello-world/src/main.zig
@@ -6,47 +6,46 @@ const GenericWriter = std.io.GenericWriter;
 pub fn main() !void {
     const stdIn = std.io.getStdIn().reader();
     const stdOut = std.io.getStdOut().writer();
-
+    // const arena =
     try writeLine(stdOut, "Let's play the guessing game. The lower bound will be 1.");
     _ = try getUpperBound(stdIn, stdOut);
 }
 
-pub fn getUpperBound(reader: anytype, writer: anytype) !u8 {
-    var inputBuffer: [INPUT_BUFFER_SIZE]u8 = undefined; // todo: Understand how to make this be more dynamic, or how to ensure that we don't exceed the buffer
+pub fn getUpperBound(reader: anytype, writer: anytype) !u32 {
+    // var inputBuffer: [INPUT_BUFFER_SIZE]u8 = undefined;
+    var inputBuffer = [_]u8{0} ** INPUT_BUFFER_SIZE; // todo: Understand how to make this be more dynamic, or how to ensure that we don't exceed the buffer
+    var validUserInput = false;
+    var upperBound: u32 = undefined;
 
-    try writeLine(writer, "What should the upper bound be? (Default 10). Just press enter to select the default value.");
+    while (!validUserInput) {
+        try writeLine(writer, "What should the upper bound be? (Default 10). Just press enter to select the default value.");
+        try readUserInputIntoBuffer(reader, &inputBuffer); //I'm worried that this will lead to leaks. However, the buffer seems to include the trailing delimiter, but no indication of how long the input was - or rather, I can't see it.
+        upperBound = std.fmt.parseInt(u32, &inputBuffer, 10) catch {
+            try writeLineWithArgs(writer, "Come on now behave. Enter a number. Greater than 0. You entered '{s}'. Try again.", .{&inputBuffer});
+            continue;
+        };
+        validUserInput = true;
+    }
 
-    try readUserInputIntoBuffer(reader, &inputBuffer);
-
-    // if too long, write a message like "too long"
-    // if cant parse as an int, then try writeSassyMessage(writer);
-    // otherwise we can return the user input
-
-    try writeLineWithArgs(writer, "you entered: {s}", .{inputBuffer});
-
-    return 42;
+    return upperBound;
 }
 
 // I don't love writer being an `anytype` here, since I know it has something to do with a `GenericWriter`, at least that's what I'm getting from the LSP. I'll have to deal with that later
 fn writeLine(writer: anytype, comptime format: []const u8) !void {
-    try writer.print("\n" ++ format ++ "\n", .{});
+    try writer.print(NEWLINE ++ format ++ NEWLINE, .{});
 }
 
 fn writeLineWithArgs(writer: anytype, comptime format: []const u8, args: anytype) !void {
-    try writer.print("\n" ++ format ++ "\n", args);
+    try writer.print(NEWLINE ++ format ++ NEWLINE, args);
 }
 
 fn readUserInputIntoBuffer(reader: anytype, inputBuffer: *[INPUT_BUFFER_SIZE]u8) !void {
-    _ = try reader.readUntilDelimiter(inputBuffer, '\n'); // return type is (NoEofError || error{StreamTooLong})![]u8.
-
-    // todo: readUntilDelimiterAlloc is the one we want I think. It's return type is (Error || Allocator.Error || error{StreamTooLong})!?[]u8
-
-}
-
-fn writeSassyMessage(writer: anytype) !void {
-    try writeLine(writer, "come on now, behave yourself. Let's try again");
+    _ = try reader.readUntilDelimiter(inputBuffer, NEWLINE_DELIMETER); // return type is (NoEofError || error{StreamTooLong})![]u8.
+    // todo: Don't include the delimeter in the output
 }
 
 const MAX_UTF_CHARACTER_BYTES: comptime_int = 4;
 const MAX_SUPPORTED_INPUT_LENGTH: comptime_int = 10;
 const INPUT_BUFFER_SIZE: comptime_int = MAX_UTF_CHARACTER_BYTES * MAX_SUPPORTED_INPUT_LENGTH;
+const NEWLINE_DELIMETER: comptime_int = '\n';
+const NEWLINE: *const [1:0]u8 = "\n";
