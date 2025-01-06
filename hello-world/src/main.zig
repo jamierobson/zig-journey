ff16ed0a1367fe7c8a7a89e172bd50535a58f2f5
Jamie Robson <jamierobson@users.noreply.github.com>
2025-01-06 23:56:29 +0100

    Strip the delimeter Identify when we have reached undefined and parse only what's left
    But now it parses a user input into an i32. Progress!

--- hello-world/src/main.zig
+++ hello-world/src/main.zig
@@ -8,26 +8,41 @@ pub fn main() !void {
     const stdOut = std.io.getStdOut().writer();
     // const arena =
     try writeLine(stdOut, "Let's play the guessing game. The lower bound will be 1.");
-    _ = try getUpperBound(stdIn, stdOut);
+    const upperBound = try getUpperBound(stdIn, stdOut);
+
+    try writeLineWithArgs(stdOut, "Ok, let's play! Guess a number between 1 and {}", .{upperBound});
 }
 
 pub fn getUpperBound(reader: anytype, writer: anytype) !u32 {
-    // var inputBuffer: [INPUT_BUFFER_SIZE]u8 = undefined;
-    var inputBuffer = [_]u8{0} ** INPUT_BUFFER_SIZE; // todo: Understand how to make this be more dynamic, or how to ensure that we don't exceed the buffer
+    var inputBuffer: [INPUT_BUFFER_SIZE]u8 = undefined;
     var validUserInput = false;
     var upperBound: u32 = undefined;
 
     while (!validUserInput) {
         try writeLine(writer, "What should the upper bound be? (Default 10). Just press enter to select the default value.");
-        try readUserInputIntoBuffer(reader, &inputBuffer); //I'm worried that this will lead to leaks. However, the buffer seems to include the trailing delimiter, but no indication of how long the input was - or rather, I can't see it.
-        upperBound = std.fmt.parseInt(u32, &inputBuffer, 10) catch {
-            try writeLineWithArgs(writer, "Come on now behave. Enter a number. Greater than 0. You entered '{s}'. Try again.", .{&inputBuffer});
+        try readUserInputIntoBuffer(reader, &inputBuffer);
+        const consumedBufferSlice = try extractUserInputSlice(&inputBuffer);
+
+        upperBound = std.fmt.parseInt(u32, consumedBufferSlice, 10) catch {
+            try writeLineWithArgs(writer, "Come on now behave. Enter a number. Greater than 0. You entered '{any}'. Try again.", .{consumedBufferSlice});
+            inputBuffer = undefined;
             continue;
         };
         validUserInput = true;
     }
 
     return upperBound;
+}
+
+fn extractUserInputSlice(buffer: []u8) ![]u8 {
+    var firstUndefinedIndex = buffer.len - 1;
+    for (buffer, 0..) |element, index| {
+        if (element == UNDEFINED_CHARACTER) {
+            firstUndefinedIndex = index;
+            break;
+        }
+    }
+    return buffer[0..firstUndefinedIndex];
 }
 
 // I don't love writer being an `anytype` here, since I know it has something to do with a `GenericWriter`, at least that's what I'm getting from the LSP. I'll have to deal with that later
@@ -41,11 +56,26 @@ fn writeLineWithArgs(writer: anytype, comptime format: []const u8, args: anytype
 
 fn readUserInputIntoBuffer(reader: anytype, inputBuffer: *[INPUT_BUFFER_SIZE]u8) !void {
     _ = try reader.readUntilDelimiter(inputBuffer, NEWLINE_DELIMETER); // return type is (NoEofError || error{StreamTooLong})![]u8.
-    // todo: Don't include the delimeter in the output
+
+    for (
+        0..inputBuffer.len,
+    ) |index| {
+        if (index == inputBuffer.len) {
+            break;
+        }
+
+        if (inputBuffer[index] == RETURN_DELIMETER and inputBuffer[index + 1] == NEWLINE_DELIMETER) {
+            inputBuffer[index] = undefined;
+            inputBuffer[index + 1] = undefined;
+            break;
+        }
+    }
 }
 
 const MAX_UTF_CHARACTER_BYTES: comptime_int = 4;
 const MAX_SUPPORTED_INPUT_LENGTH: comptime_int = 10;
 const INPUT_BUFFER_SIZE: comptime_int = MAX_UTF_CHARACTER_BYTES * MAX_SUPPORTED_INPUT_LENGTH;
 const NEWLINE_DELIMETER: comptime_int = '\n';
+const RETURN_DELIMETER: comptime_int = '\r';
 const NEWLINE: *const [1:0]u8 = "\n";
+const UNDEFINED_CHARACTER: comptime_int = 0xAA;
