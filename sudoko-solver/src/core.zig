adadec4ba8d69b170ba2870a4184eeeceb1cb1dc
Jamie Robson <jamierobson@users.noreply.github.com>
2025-01-11 16:29:35 +0100

    Basic grid coming to shape. Have the game, the grid, and the views. That was easy enough, no Rc<RefCell<T>> needed :phew:

--- sudoko-solver/src/core.zig
+++ sudoko-solver/src/core.zig
@@ -3,15 +3,15 @@ const std = @import("std");
 const Allocator = std.mem.Allocator;
 
 pub const Cell = struct {
-    value: ?u8,
-    discountedValues: std.ArrayList(u8),
+    value: ?usize,
+    discountedValues: std.ArrayList(usize),
 
     pub fn initEmpty(allocator: Allocator) Cell {
         return initFromValue(null, allocator);
     }
 
-    pub fn initFromValue(value: ?u8, allocator: Allocator) Cell {
-        return Cell{ .value = value, .discountedValues = std.ArrayList(u8).init(allocator) };
+    pub fn initFromValue(value: ?usize, allocator: Allocator) Cell {
+        return Cell{ .value = value, .discountedValues = std.ArrayList(usize).init(allocator) };
     }
 
     pub fn deinit(self: *Cell) void {
@@ -21,15 +21,41 @@ pub const Cell = struct {
 
 pub const CellGrid = [consts.PUZZLE_DIMENTION][consts.PUZZLE_DIMENTION]Cell;
 
-pub const ValidatableGroup = [consts.PUZZLE_DIMENTION]Cell;
+pub const ValidatableGroup = struct {
+    identifier: usize,
+    members: [consts.PUZZLE_DIMENTION]*Cell,
+};
 
 pub const Views = struct {
-    rows: [consts.PUZZLE_DIMENTION]*Cell,
-    columns: [consts.PUZZLE_DIMENTION]*Cell,
-    blocks: [consts.PUZZLE_DIMENTION]*Cell,
+    rows: [consts.PUZZLE_DIMENTION]ValidatableGroup,
+    columns: [consts.PUZZLE_DIMENTION]ValidatableGroup,
+    blocks: [consts.PUZZLE_DIMENTION]ValidatableGroup,
 };
 
 pub const SudokuPuzzle = struct {
     grid: CellGrid,
     views: Views,
+
+    pub fn initEmpty(allocator: Allocator) SudokuPuzzle {
+        var grid: CellGrid = undefined;
+        var views = Views{ .rows = undefined, .columns = undefined, .blocks = undefined };
+
+        for (0..consts.PUZZLE_MAXIMUM_VALUE - 1) |i| {
+            views.rows[i] = ValidatableGroup{ .identifier = i, .members = undefined };
+            views.columns[i] = ValidatableGroup{ .identifier = i, .members = undefined };
+            views.blocks[i] = ValidatableGroup{ .identifier = i, .members = undefined };
+        }
+
+        for (0..consts.PUZZLE_MAXIMUM_VALUE - 1) |i| {
+            for (0..consts.PUZZLE_MAXIMUM_VALUE - 1) |j| {
+                var cell = Cell.initEmpty(allocator);
+                grid[i][j] = cell;
+                views.rows[i].members[j] = &cell;
+                views.columns[j].members[i] = &cell;
+                views.blocks[i].members[j] = &cell;
+            }
+        }
+
+        return SudokuPuzzle{ .grid = grid, .views = views };
+    }
 };
--- sudoko-solver/src/main.zig
+++ sudoko-solver/src/main.zig
@@ -1,10 +1,20 @@
 const std = @import("std");
 const core = @import("core.zig"); // todo: There must be better than this?
+const consts = @import("consts.zig");
 
 pub fn main() !void {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     const allocator = gpa.allocator();
     const cell = core.Cell.initFromValue(3, allocator);
+    const puzzle = core.SudokuPuzzle.initEmpty(allocator);
 
-    std.debug.print("cell was created with value {any}", .{cell.value});
+    std.debug.print("cell was created with value {any} \n", .{cell.value});
+
+    for (0..consts.PUZZLE_MAXIMUM_VALUE - 1) |i| {
+        std.debug.print("\n", .{});
+
+        for (0..consts.PUZZLE_MAXIMUM_VALUE - 1) |j| {
+            std.debug.print("| {} |", .{puzzle.grid[i][j].value orelse 0});
+        }
+    }
 }
