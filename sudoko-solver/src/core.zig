9630af2d1e5f2ac7d02113cdb1f5b69a5c51f4fc
Jamie Robson <jamierobson@users.noreply.github.com>
2025-01-12 22:48:44 +0100

    Using an arena allocator, before delving deper into proper destruction

--- sudoko-solver/src/core.zig
+++ sudoko-solver/src/core.zig
@@ -36,9 +36,11 @@ pub const SudokuPuzzle = struct {
     grid: CellGrid,
     views: Views,
 
-    pub fn initEmpty(allocator: Allocator) !*SudokuPuzzle {
+    pub fn create(allocator: Allocator) !*SudokuPuzzle {
         var puzzle = try allocator.create(SudokuPuzzle);
-        errdefer allocator.destroy(puzzle);
+        errdefer {
+            allocator.destroy(puzzle);
+        }
 
         for (0..consts.PUZZLE_MAXIMUM_VALUE) |row| {
             const identifier = row; // for reusing the iteration
@@ -60,47 +62,12 @@ pub const SudokuPuzzle = struct {
         return puzzle;
     }
 
-    pub fn deinit(self: SudokuPuzzle, allocator: Allocator) void {
-        for (self.grid) |row| {
-            for (row) |cell| {
+    pub fn destroy(self: *SudokuPuzzle, allocator: Allocator) void {
+        for (self.grid) |cellRow| {
+            for (cellRow) |cell| {
                 cell.deinit();
             }
         }
-
         allocator.destroy(self);
-    }
-
-    pub fn initEmpty_flawed(allocator: Allocator) SudokuPuzzle {
-
-        // Keeping this here as a comparison of something I learned.
-
-        var views = Views{ .rows = undefined, .columns = undefined, .blocks = undefined };
-
-        for (0..consts.PUZZLE_MAXIMUM_VALUE) |row| {
-            const identifier = row; // for reusing the iteration
-            views.rows[identifier] = ValidatableGroup{ .identifier = identifier, .members = undefined }; //todo: I can probably drop that identifier now. It's all identifiable by position in the collections
-            views.columns[identifier] = ValidatableGroup{ .identifier = identifier, .members = undefined };
-            views.blocks[identifier] = ValidatableGroup{ .identifier = identifier, .members = undefined };
-        }
-
-        // todo: I would like this to not have to be a second loop. The thing is that the columns collections aren't set in time in the first loop. We can make a choice to accept that later. For now, i'd like
-        // rows and columns to look like the underlying grid, where possible, at least as I learn to interact with memory management
-
-        // I'm getting stuck here. It doesn't look like the pointer setup I have alters values correctly, or reads them so either.
-        // The goal is that each row, column, block, can use the cell, for example, when it works out if there are any duplicates among the cells in that group. Recalculating those groups all the time will be costly
-        // and I want to explore making sure that all of the memory is correctly accounted for.
-
-        var grid: CellGrid = undefined;
-
-        for (0..consts.PUZZLE_MAXIMUM_VALUE) |row| {
-            for (0..consts.PUZZLE_MAXIMUM_VALUE) |column| {
-                grid[row][column] = Cell.initEmpty(allocator);
-                views.rows[row].members[column] = &grid[row][column];
-                views.columns[column].members[row] = &grid[row][column];
-                views.blocks[row].members[column] = &grid[row][column];
-            }
-        }
-
-        return SudokuPuzzle{ .grid = grid, .views = views };
     }
 };
--- sudoko-solver/src/main.zig
+++ sudoko-solver/src/main.zig
@@ -4,10 +4,11 @@ const consts = @import("consts.zig");
 
 pub fn main() !void {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
-    const allocator = gpa.allocator();
-    var puzzle = try core.SudokuPuzzle.initEmpty(allocator);
-    // defer puzzle.deinit(allocator);
-    defer _ = gpa.deinit();
+    var arena = std.heap.ArenaAllocator.init(gpa.allocator());
+    defer arena.deinit();
+
+    var puzzle = try core.SudokuPuzzle.create(arena.allocator());
+    defer puzzle.destroy(arena.allocator());
 
     puzzle.grid[1][1].value = 5;
     puzzle.views.columns[0].members[0].*.value = 1;
@@ -24,15 +25,4 @@ pub fn main() !void {
             std.debug.print("| Grid: {any}, R{any}: {any}, C{any}: {any} |", .{ puzzle.grid[row][column].value orelse 0, rowView.identifier, rowView.members[column].value orelse 0, columnView.identifier, columnView.members[row].value orelse 0 });
         }
     }
-
-    // // try use after free
-    // puzzle.deinit();
-    // _ = gpa.deinit();
-
-    // puzzle.grid[3][3].value = 8;
-    // std.debug.print(" \n \n Try use after free. 3, 3 = {any} \n ", .{puzzle.grid[3][3].value}); // interesting that this works just fine.
-
-    // // try double free
-    // puzzle.deinit();
-    // _ = gpa.deinit();
 }
